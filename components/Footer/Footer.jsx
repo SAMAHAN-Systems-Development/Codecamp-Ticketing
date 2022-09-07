d775ed4dcb09fb48477beddca6564fad696b219b
viver <viverbungag1@gmail.com>
2022-09-07 20:48:25 +0800

    Finished footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import styles from "./Footer.module.scss";
+import Image from 'next/image';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className={styles["footer"]}>
+      <div className={styles["footer__content"]}>
+        <section className={styles["footer__left-section"]}>
+          <div className={styles["footer__title"]}>
+            <h2>A SAMAHAN SYSTEMS DEVELOPMENT 2022 PROJECT</h2>
+          </div>
+          <div className={styles["footer__caption"]}>
+            <h3>GOT ANY QUESTIONS? REACH OUT TO US!</h3>
+          </div>
+          <div className={styles["footer__contacts"]}>
+            <div className={styles["footer__contacts-item"]}>
+              <h4>SOCIALS</h4>
+              <h4>/</h4>
+              <h4>@SAMAHANSysDev</h4>
+            </div>
+            <div className={styles["footer__contacts-item"]}>
+              <h4>EMAIL</h4>
+              <h4>/</h4>
+              <h4>samahan.sd@addu.edu.ph</h4>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles["footer__right-section"]}>
+          <div className={styles["footer__logo-sticker"]}>
+            <Image
+              src="/assets/images/SysDev Smiley Sticker.png"
+              alt="SysDev Logo Sticker"
+              layout="fill"
+              objectFit="contain"
+              draggable={false}
+              priority
+            />
+          </div>
+          <div className={styles["footer__logo-text"]}>
+            <h4>SYS</h4>
+            <h4>DEV</h4>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
